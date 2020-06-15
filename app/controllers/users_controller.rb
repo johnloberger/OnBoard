503a50912b6d9033e66e8a03d4448e1ae04f28e6
emilioquintana90 <emilioquintana90@gmail.com>
2020-06-15 17:00:53 -0500

    User Welcome Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :pets]
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+
+    if @user.valid?
+      redirect_to user_path(@user)
+    else
+      flash[:user_errors] = @user.errors.full_messages
+      redirect_to new_user_path(@user)
+    end
+
+  end
+
+  def pets
+    @pets = @user.pets
+    render :'pets'
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age, :email_address)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
